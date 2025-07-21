1360b9a783271a044d83c25da856ae9ecf08c067
Muhammad Hudaibi <muhhudaibi11@gmail.com>
2025-07-21 14:32:57 +0700

    initial commit

---
+++ src/main/java/com/enigmacamp/assesment/AssessmentSouceCodeMuhammadhudaibiApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentSouceCodeMuhammadhudaibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentSouceCodeMuhammadhudaibiApplication.class, args);
+	}
+
+}
